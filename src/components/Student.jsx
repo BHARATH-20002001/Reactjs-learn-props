40703d784ad30975d5dd3bb544272d6f15af9b76
BARATH-20002001 <bharathsarangan1@gmail.com>
2024-02-13 13:33:26 +0530

    first commit

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,22 +1,31 @@
 import "./Student.css";
+import propTypes from "prop-types";
 
 export const Student = (props) => {
   return (
     <div className="student">
       <table>
-        <tr>
-          <th>Name</th>
-          <td>{props.Name}</td>
-        </tr>
-        <tr>
-          <th>Age</th>
-          <td>{props.Age}</td>
-        </tr>
-        <tr>
-          <th>isMarried</th>
-          <td>{props.isMarried?"Yes" : "No"}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <td>{props.Name}</td>
+          </tr>
+          <tr>
+            <th>Age</th>
+            <td>{props.Age}</td>
+          </tr>
+          <tr>
+            <th>isMarried</th>
+            <td>{props.isMarried ? "Yes" : "No"}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
+};
+
+Student.propTypes = {
+  Name: propTypes.string,
+  Age: propTypes.number,
+  isMarried: propTypes.bool,
 };
